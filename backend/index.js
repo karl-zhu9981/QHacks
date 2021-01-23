719601854c2d6d977ca11e0ef0fa970c92fb7e04
ChingChang9 <chingtheprogrammer@gmail.com>
2021-01-23 17:15:22 -0500

    node post server for website

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,19 @@
 const getSong = require("./getSong.js");
+const http = require("http");
 
-getSong("alexander hamilton");
+http.createServer((request, response) => {
+  response.writeHead(200, {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*"
+  });
+
+  let chunks = [];
+
+  request.on("data", chunk => chunks.push(chunk));
+  request.on("end", () => {
+    const responseBody = Buffer.concat(chunks).toString();
+    chunks = null;
+    getSong(responseBody);
+  });
+}).listen(8000);
+console.log("Server running on port 8000");
