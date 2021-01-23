25d53a5f911256ef5cdd6499466f6b9f5e780519
abbygklui <63834576+abbygklui@users.noreply.github.com>
2021-01-23 17:56:23 -0500

    Update Home.vue

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <img alt="Landing Image" src="../assets/landingimage.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
