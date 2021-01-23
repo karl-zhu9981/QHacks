c096f5c68b6e298940e5373f8b977dd0f8fb6034
ChingChang9 <chingtheprogrammer@gmail.com>
2021-01-23 15:20:39 -0500

    moved the whole site to the frontend folder (backend folder coming soon)

---
+++ frontend/src/views/Convert.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="about">
+    <input v-model="searchString" type="text" placeholder="Paste a Youtube Url..." />
+    <button @click="downloadSong(searchString)">Convert</button>
+    <div v-if="error">{{ errorMessage }}</div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "Convert",
+  data() {
+    return {
+      searchString: "",
+      errorMessage: "",
+      error: false
+    }
+  },
+  methods: {
+    async downloadSong(searchString) {
+      // make a post request to our backend
+    }
+  }
+};
+</script>
